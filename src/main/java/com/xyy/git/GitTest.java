8c8123baf84b6ad5a0c344311c62dae44b3c0820
XYY <XYY@xyy.com>
2023-01-03 22:24:13 +0800

    push commit test3

--- src/main/java/com/xyy/git/GitTest.java
+++ src/main/java/com/xyy/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("master-test");
         System.out.println("push test0");
+        System.out.println("ddddd");
     }
 }
