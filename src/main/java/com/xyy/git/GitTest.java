bd9772937f3123b7bb8afb0118971d603c866f45
XYY <XYY@xyy.com>
2023-01-03 21:16:34 +0800

    second commit

--- src/main/java/com/xyy/git/GitTest.java
+++ src/main/java/com/xyy/git/GitTest.java
@@ -3,5 +3,6 @@ package com.xyy.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("yonggezhenshuai");
+        System.out.println("sssssss");
     }
 }
